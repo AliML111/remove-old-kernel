f496e8eca6a524f0d301a7175cf69fab73ca795c
BlackGhostUK1 <85413150+BlackGhostUK1@users.noreply.github.com>
2023-04-20 11:43:45 +0330

    Add files via upload

---
+++ rm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rpm -qa kernel | cat -n
+CHECK=$(rpm -qa kernel | wc -l)
+read -rp "which kernel you wanna remove? " NUM
+while [ "$NUM" -lt "$CHECK" ] || [ "$NUM" -eq "$CHECK" ]; do
+        VER=$(rpm -qa kernel | cut -d "-" -f 2,3 | tr "[:space:]" ":" | cut -d ":" -f "$NUM")
+        if sudo dnf remove kernel-core-"$VER" -y &> /dev/null; sudo dnf remove kernel-devel-"$VER" -y &> /dev/null;then
+	      echo "completed!"
+       else
+		echo "no!"
+ 	fi
+	break;
+done
+
+
