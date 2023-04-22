c4f8f2d8f082dbc545c70265776d9a175d4d9ab1
BlackGhostUK1 <85413150+BlackGhostUK1@users.noreply.github.com>
2023-04-22 10:23:48 +0330

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -5,9 +5,9 @@ read -rp "which kernel you wanna remove? " NUM
 while [ "$NUM" -lt "$CHECK" ] || [ "$NUM" -eq "$CHECK" ]; do
         VER=$(rpm -qa kernel | cut -d "-" -f 2,3 | tr "[:space:]" ":" | cut -d ":" -f "$NUM")
         if sudo dnf remove kernel-core-"$VER" -y &> /dev/null; sudo dnf remove kernel-devel-"$VER" -y &> /dev/null;then
-	      echo "completed!"
+	      echo "====completed!===="
        else
-		echo "no!"
+		echo "====Error!===="
  	fi
 	break;
 done
